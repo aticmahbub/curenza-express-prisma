ce5a9eb43c5cdeec88d05634376b18eb1f9927fa
aticcmahbub <aticcmahbub@gmail.com>
2025-11-24 20:08:34 +0600

    feat: add create patient functionality

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,16 @@
 import type {Request, Response} from 'express';
 import {UserService} from './user.service';
 import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
     const result = await UserService.createPatient(req.body);
-    res.send(result);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Patient is created successfully',
+        data: result,
+    });
 });
 
 export const UserController = {createPatient};
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,24 @@
 import config from '../../../config';
 import bcrypt from 'bcryptjs';
 import type {CreatePatientInput} from './user.types';
+import {prisma} from '../../../lib/prisma';
 
 const createPatient = async (payload: CreatePatientInput) => {
-    return payload;
+    const hashedPassword = await bcrypt.hash(
+        payload.password,
+        Number(config.bcrypt_js_salt_round),
+    );
+
+    const result = await prisma.$transaction(async (tnx) => {
+        await tnx.user.create({
+            data: {email: payload.email, password: hashedPassword},
+        });
+
+        return await tnx.patient.create({
+            data: {name: payload.name, email: payload.email},
+        });
+    });
+    return result;
 };
 
 export const UserService = {createPatient};
