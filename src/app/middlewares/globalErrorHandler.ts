378ff7f73d6c4a843891c9c81c8847b74569e493
aticcmahbub <aticcmahbub@gmail.com>
2025-12-11 23:09:49 +0600

    implement prisma error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,4 +1,5 @@
 import type {NextFunction, Request, Response} from 'express';
+import {Prisma} from '../../generated/client';
 
 const globalErrorHandler = (
     err: any,
@@ -10,6 +11,36 @@ const globalErrorHandler = (
     let success = false;
     let message = err.message || 'Something went wrong!';
     let error = err;
+
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+            message = 'Duplicate key error';
+            error = err.meta;
+            statusCode = 409;
+        }
+        if (err.code === 'P1000') {
+            message = 'Authentication error';
+            error = err.meta;
+            statusCode = 502;
+        }
+        if (err.code === 'P2003') {
+            message = 'Foreign key constraint failed';
+            error = err.meta;
+            statusCode = 502;
+        }
+    } else if (err instanceof Prisma.PrismaClientValidationError) {
+        message = 'Validation error';
+        error = err.message;
+        statusCode = 402;
+    } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+        message = 'Unknown prisma error occurred';
+        error = err.message;
+        statusCode = 402;
+    } else if (err instanceof Prisma.PrismaClientInitializationError) {
+        message = 'Prisma client failed to initialized';
+        error = err.message;
+        statusCode = 402;
+    }
 
     res.status(statusCode).json({
         success,
