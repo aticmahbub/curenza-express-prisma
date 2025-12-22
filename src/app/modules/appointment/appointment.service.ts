5e85516fb782be9e1e150f231c5dac15ea70a8c3
aticcmahbub <aticcmahbub@gmail.com>
2025-12-22 16:36:07 +0600

    feat: create function to delete unpaid appointments

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -6,6 +6,7 @@ import {v4 as uuid} from 'uuid';
 import {calculatePagination} from '../../utils/pagination';
 import {
     AppointmentStatus,
+    PaymentStatus,
     UserRole,
     type Prisma,
 } from '../../../generated/client';
@@ -159,8 +160,46 @@ const updateAppointmentStatus = async (
     });
 };
 
+const cancelUnpaidAppointMents = async () => {
+    const triggerTime = new Date(Date.now() - 30 * 60 * 1000);
+
+    const unpaidAppointMents = await prisma.appointment.findMany({
+        where: {
+            createdAt: {lte: triggerTime},
+            paymentStatus: PaymentStatus.UNPAID,
+        },
+    });
+
+    const appointmentIdsToCancel = unpaidAppointMents.map(
+        (appointment) => appointment.id,
+    );
+
+    await prisma.$transaction(async (tnx) => {
+        await tnx.payment.deleteMany({
+            where: {appointmentId: {in: appointmentIdsToCancel}},
+        });
+
+        await tnx.appointment.deleteMany({
+            where: {id: {in: appointmentIdsToCancel}},
+        });
+
+        for (const unpaidAppointMent of unpaidAppointMents) {
+            await tnx.doctorSchedule.update({
+                where: {
+                    doctorId_scheduleId: {
+                        doctorId: unpaidAppointMent.doctorId,
+                        scheduleId: unpaidAppointMent.scheduleId,
+                    },
+                },
+                data: {isBooked: false},
+            });
+        }
+    });
+};
+
 export const AppointmentService = {
     createAppointment,
     getMyAppointments,
     updateAppointmentStatus,
+    cancelUnpaidAppointMents,
 };
